f41e76583cf18807a30203ee2523a0ab7a5edbde
Kidus <kidusm7110@gmail.com>
2025-08-13 23:44:04 +0300

    fill attribute option with more generic terms --> Seeder

--- database/seeders/AttributeSeeder.php
+++ database/seeders/AttributeSeeder.php
@@ -17,14 +17,49 @@ class AttributeSeeder extends Seeder
         // Attribute Types
         $color = AttributeType::create(['name' => 'Color']);
         $size  = AttributeType::create(['name' => 'Size']);
+        $material = AttributeType::create(['name' => 'Material']);
+        $style = AttributeType::create(['name' => 'Style']);
+        $pattern = AttributeType::create(['name' => 'Pattern']);
+        $fit = AttributeType::create(['name' => 'Fit']);
 
         // Attribute Options
         AttributeOption::insert([
+            // Colors
             ['attribute_type_id' => $color->id, 'value' => 'Red'],
             ['attribute_type_id' => $color->id, 'value' => 'Blue'],
+            ['attribute_type_id' => $color->id, 'value' => 'Green'],
+            ['attribute_type_id' => $color->id, 'value' => 'Black'],
+            ['attribute_type_id' => $color->id, 'value' => 'White'],
+
+            // Sizes
+            ['attribute_type_id' => $size->id, 'value' => 'XS'],
             ['attribute_type_id' => $size->id, 'value' => 'S'],
             ['attribute_type_id' => $size->id, 'value' => 'M'],
             ['attribute_type_id' => $size->id, 'value' => 'L'],
+            ['attribute_type_id' => $size->id, 'value' => 'XL'],
+
+            // Materials
+            ['attribute_type_id' => $material->id, 'value' => 'Cotton'],
+            ['attribute_type_id' => $material->id, 'value' => 'Wool'],
+            ['attribute_type_id' => $material->id, 'value' => 'Polyester'],
+            ['attribute_type_id' => $material->id, 'value' => 'Leather'],
+
+            // Styles
+            ['attribute_type_id' => $style->id, 'value' => 'Casual'],
+            ['attribute_type_id' => $style->id, 'value' => 'Formal'],
+            ['attribute_type_id' => $style->id, 'value' => 'Sport'],
+            ['attribute_type_id' => $style->id, 'value' => 'Vintage'],
+
+            // Patterns
+            ['attribute_type_id' => $pattern->id, 'value' => 'Solid'],
+            ['attribute_type_id' => $pattern->id, 'value' => 'Striped'],
+            ['attribute_type_id' => $pattern->id, 'value' => 'Checked'],
+            ['attribute_type_id' => $pattern->id, 'value' => 'Printed'],
+
+            // Fit
+            ['attribute_type_id' => $fit->id, 'value' => 'Slim'],
+            ['attribute_type_id' => $fit->id, 'value' => 'Regular'],
+            ['attribute_type_id' => $fit->id, 'value' => 'Loose'],
         ]);
     }
 }
