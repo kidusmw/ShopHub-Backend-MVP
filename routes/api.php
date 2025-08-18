4a11e714c590c4fe4cdb3c125c28a7fdd22214ac
Kidus <kidusm7110@gmail.com>
2025-08-18 19:32:52 +0300

    Revert "Create Authentication controller"
    This reverts commit 69bf3df1536ef0dfb9d0b1204f84ec7907b28967.

--- routes/api.php
+++ routes/api.php
@@ -8,18 +8,16 @@ use App\Http\Controllers\Api\CartController;
 use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\AuthController;
 
-/**
- * Auth routes (Protected Routes)
- * */
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
 });
 
 /**
- * Auth routes (Public Routes)
+ * Auth routes
  * */
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
