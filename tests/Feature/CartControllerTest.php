1bc9240bd456d6a9e899e2ee774f3d478a3164ff
Kidus <kidusm7110@gmail.com>
2025-08-24 21:51:40 +0300

    Create testing env, Change testing config from refresh database to database transaction

--- tests/Feature/CartControllerTest.php
+++ tests/Feature/CartControllerTest.php
@@ -1,7 +1,27 @@
 <?php
 
-test('example', function () {
-    $response = $this->get('/');
+use App\Models\User;
+use App\Models\Product;
+use App\Models\Cart;
+use App\Models\CartItem;
+use App\Models\Variant;
 
-    $response->assertStatus(200);
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+use function Pest\Laravel\{getJson, actingAs};
+
+uses(TestCase::class, DatabaseTransactions::class)->in('Feature');
+
+it('returns 401 if no active cart exists', function () {
+    // Create a user
+    $user = User::factory()->create();
+
+    // Act as the created user and make a GET request to the cart endpoint
+    actingAs($user, 'sanctum');
+
+    // Ensure no active cart exists for the user
+    getJson('/api/cart')
+        ->assertStatus(404)
+        ->assertJson(['message' => 'No active cart found']);
 });
